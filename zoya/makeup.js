f394e9735aa53e53ff111503b8e34719070c7ee7
ZOYA <126981793+Zoyaaaaaaa@users.noreply.github.com>
2024-07-15 20:58:56 +0530

    Add files via upload

---
+++ zoya/makeup.js
@@ -0,0 +1,30 @@
+// makeup.js
+
+const makeupData = {
+    "Table 1": [
+      {"High End Makeup": "1. Tatcha The Silk Canvas Protective Primer", "Drugstore Makeup Dupe": "e.l.f. Poreless Putty Primer"},
+      {"High End Makeup": "2. Giorgio Armani Luminous Silk Foundation", "Drugstore Makeup Dupe": "L’Oreal True Match Super-Blendable Foundation"},
+      {"High End Makeup": "3. Tarte Shape Tape Contour Concealer", "Drugstore Makeup Dupe": "Maybelline Instant Age Rewind Eraser Dark Circle Treatment Concealer"},
+      {"High End Makeup": "4. Charlotte Tilbury Airbrush Matte Bronzer", "Drugstore Makeup Dupe": "NYX Matte Bronzer"},
+      {"High End Makeup": "5. NARS Blush", "Drugstore Makeup Dupe": "Milani Baked Blush"},
+      {"High End Makeup": "6. Urban Decay Naked3 Palette Eyeshadow Palette", "Drugstore Makeup Dupe": "CoverGirl Roses TruNaked Eyeshadow Palette"},
+      {"High End Makeup": "7. It Cosmetics Brow Power Micro Universal Defining Eyebrow Pencil", "Drugstore Makeup Dupe": "Maybelline Brow Ultra Slim Defining Eyebrow Pencil"},
+      {"High End Makeup": "8. Too Faced Better Than Sex Mascara", "Drugstore Makeup Dupe": "L’Oreal Voluminous Lash Paradise Mascara"},
+      {"High End Makeup": "9. Fenty Gloss Bomb Universal Lip Luminizer", "Drugstore Makeup Dupe": "Revlon Super Lustrous Lip Gloss"},
+      {"High End Makeup": "10. Smashbox X BECCA Cosmetics Shimmering Skin Perfector Pressed Highlighter", "Drugstore Makeup Dupe": "Wet N Wild MegaGlo Highlighting Powder"},
+      {"High End Makeup": "11. Laura Mercier Translucent Loose Setting Powder", "Drugstore Makeup Dupe": "E.l.f. High Definition Powder"},
+      {"High End Makeup": "12. NARS Radiant Creamy Concealer", "Drugstore Makeup Dupe": "NYX HD Photogenic Concealer"},
+      {"High End Makeup": "13. Charlotte Tilbury Hollywood Flawless Filter", "Drugstore Makeup Dupe": "E.l.f. Halo Glow Liquid Filter"},
+      {"High End Makeup": "14. IT Cosmetics CC+ Cream with SPF 50+", "Drugstore Makeup Dupe": "E.l.f. Camo CC Cream"},
+      {"High End Makeup": "15. Clinique Almost Lipstick in Black Honey", "Drugstore Makeup Dupe": "e.l.f. Sheer Slick Lipstick"},
+      {"High End Makeup": "16. Tatcha The Liquid Silk Canvas Featherweight Protective Primer", "Drugstore Makeup Dupe": "E.l.f. Liquid Poreless Putty Primer"},
+      {"High End Makeup": "17. Estee Lauder Double Wear Stay-in-Place Foundation", "Drugstore Makeup Dupe": "Wet N Wild Photofocus Matte Foundation"},
+      {"High End Makeup": "18. Dior Rosy Glow Blush", "Drugstore Makeup Dupe": "Persona Super Blush"},
+      {"High End Makeup": "19. Charlotte Tilbury Pillow Talk Lipstick", "Drugstore Makeup Dupe": "E.l.f O Face Satin Lipstick"},
+      {"High End Makeup": "20. Charlotte Tilbury Pillow Talk Lip Liner", "Drugstore Makeup Dupe": "Milani Understatement Lipliner Pencil"},
+      {"High End Makeup": "21. Glossier Boy Brow", "Drugstore Makeup Dupe": "E.l.f. Wow Brow Gel"},
+      {"High End Makeup": "22. Charlotte Tilbury Airbrush Flawless Finish Setting Powder", "Drugstore Makeup Dupe": "Flower Beauty Light Illusion Perfecting Powder"}
+    ]
+  };
+  
+  
---
+++ zoya/perfume.js
@@ -0,0 +1,34 @@
+const perfumeData = [
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/Dossier-Ambery-Saffron.webp?w=1460",
+      "text": "Most similar to YSL Black Opium: Oakcha Midnight Nymph \u2014 $35.00"
+    },
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/Finery-Not-Another-Cherry.jpg?w=1460",
+      "text": "Most similar to Le Labo Santal 33: Cremo Palo Santo Eau de Toilette \u2014 $24.00"
+    },
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/Sol-de-Janeiro-Brazilian-Crush-Cheirosa-68-Beija-Flor-Fragrance-Mist.webp?w=1460",
+      "text": "Most similar to KAYALI Vanilla 28: Bath and Body Works Viva Vanilla \u2014 $19.00"
+    },
+    // {
+    //   "imageURL": null,
+    //   "text": "Most similar to Kilian Love, Don\u2019t Be Shy: ALT Fragrances Bold Love \u2014 $60.00"
+    // },
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/Oakcha-Midnight-Nymph.webp?w=1460",
+      "text": "Most similar to Glossier You: Le Monde Gourmand Fleur de Blonde Eau de Parfum \u2014 $25.00"
+    },
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/Cremo-Palo-Santo-Eau-de-Toilette.jpg?w=1460",
+      "text": "Most similar to Jo Malone Wood Sage + Sea Salt: Aromapassions Nature \u2014 $79.00"
+    },
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/Bath-and-Body-Works-Viva-Vanilla.jpg?w=1460",
+      "text": "Most similar to Giorgio Armani My Way: Zara Sublime Epoque \u2014 $38.00"
+    },
+    {
+      "imageURL": "https://www.wellandgood.com/wp-content/uploads/2024/06/ALT-Fragrances-Bold-Love.webp?w=1460",
+      "text": "Most similar to Lake & Skye Santal Gray Eau de Parfum: Pacifica Neon Moon Spray Perfume \u2014 $22.00"
+    }
+  ];
